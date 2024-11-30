8c0ebaac383855b265caff8c4a7e1ba89590878d
Ryxusnine <ryanmizarwiradinata@gmail.com>
2024-11-30 17:11:52 +0800

    profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.navbar')
+@section('content')
+    <div class="container p-5 bg-white m-5 mx-auto rounded">
+        <label for="firstName" class="form-label">First Name</label>
+        <input class="form-control mb-3" type="text" id="firstName" name="firstName" value="John">
+        <label for="email" class="form-label">Email</label>
+        <input class="form-control" type="text" id="email" name="email" value="John@example.com">
+        <button class="mt-3 btn btn-primary">Update</button>
+    </div>
+
+    <div class="container p-5 bg-white m-5 mx-auto rounded">
+        <label for="password" class="form-label">Password</label>
+        <input class="form-control mb-3" type="text" id="password" name="password" value="John">
+        <label for="confirm" class="form-label">Confirm Password</label>
+        <input class="form-control" type="text" id="confirm" name="confirm" value="John@example.com">
+        <button class="mt-3 btn btn-primary">Update</button>
+    </div>
+@endsection
