4dd576dda30a8e500bd450a3a3fab45fff0bb2c6
abela-a <abelardhana96@gmail.com>
2024-12-14 19:50:23 +0800

    add Livewire components for admin and user questionnaire views

---
+++ app/Livewire/Questionnaire/Index/Admin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Questionnaire\Index;
+
+use Livewire\Component;
+
+class Admin extends Component
+{
+    public function render()
+    {
+        return view('livewire.questionnaire.index.admin');
+    }
+}
---
+++ app/Livewire/Questionnaire/Index/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Questionnaire\Index;
+
+use Livewire\Component;
+
+class User extends Component
+{
+    public function render()
+    {
+        return view('livewire.questionnaire.index.user');
+    }
+}
