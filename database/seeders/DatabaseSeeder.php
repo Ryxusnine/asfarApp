53b4409b56b1381ba6b2b3bed75787e102583529
abela-a <abelardhana96@gmail.com>
2024-12-16 20:14:02 +0800

    disabled dummy seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            DummySeeder::class,
+            // DummySeeder::class,
         ]);
     }
 }
